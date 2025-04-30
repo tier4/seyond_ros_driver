b87ec002adea606fdd22cc945367dc6c6fa672a9
Shinnosuke Hirakawa <shinnosuke.hirakawa@tier4.jp>
2025-04-30 16:06:51 +0900

    refactor: remove ROS1 support files

--- src/seyond_lidar_ros/node/seyond_node.cc
+++ src/seyond_lidar_ros/node/seyond_node.cc
@@ -8,33 +8,18 @@
 
 #include <signal.h>
 #include <memory>
-#ifdef ROS_FOUND
-#include <ros/ros.h>
-#include <ros/package.h>
-#include "src/driver/ros1_driver_adapter.hpp"
-#elif ROS2_FOUND
 #include <rclcpp/rclcpp.hpp>
 #include "src/driver/ros2_driver_adapter.hpp"
-#endif
 
 static void shutdown_callback(int sig) {
-#ifdef ROS_FOUND
-  ros::shutdown();
-#elif ROS2_FOUND
   rclcpp::shutdown();
-#endif
 }
 
 
 int main(int argc, char *argv[]) {
-#ifdef ROS_FOUND
-  ros::init(argc, argv, "seyond", ros::init_options::NoSigintHandler);
-#elif ROS2_FOUND
   rclcpp::init(argc, argv);
-#endif
 
   signal(SIGINT, shutdown_callback);
-
   std::shared_ptr<ROSNode> ros_driver_ptr = std::make_shared<ROSNode>();
   ros_driver_ptr->init();
   ros_driver_ptr->start();
--- src/seyond_lidar_ros/node/seyond_test.cc
+++ src/seyond_lidar_ros/node/seyond_test.cc
@@ -8,30 +8,16 @@
 
 #include <signal.h>
 #include <memory>
-#ifdef ROS_FOUND
-#include <ros/ros.h>
-#include <ros/package.h>
-#include "src/test/ros1_test.hpp"
-#elif ROS2_FOUND
 #include <rclcpp/rclcpp.hpp>
 #include "src/test/ros2_test.hpp"
-#endif
 
 static void shutdown_callback(int sig) {
-#ifdef ROS_FOUND
-  ros::shutdown();
-#elif ROS2_FOUND
   rclcpp::shutdown();
-#endif
 }
 
 
 int main(int argc, char *argv[]) {
-#ifdef ROS_FOUND
-  ros::init(argc, argv, "test", ros::init_options::NoSigintHandler);
-#elif ROS2_FOUND
   rclcpp::init(argc, argv);
-#endif
 
   signal(SIGINT, shutdown_callback);
 
